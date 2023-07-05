55476e9db83a3c4f5343f113542769b2014b9a8c
Valkon <96625039+Nawija@users.noreply.github.com>
2023-07-05 21:17:48 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `/`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "949a6d3042f4035b35f2f0631e3fb3"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
