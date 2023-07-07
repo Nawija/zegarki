99ba44d746193beadd7ae65b76f2063fc386f145
Valkon <96625039+Nawija@users.noreply.github.com>
2023-07-07 16:27:40 +0200

    ddd

--- src/components/liveBackground.js
+++ src/components/liveBackground.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 
-const LiveBacground = () => (
-    <>
-        <div class="animatemeR opacity-30">
-            <ul class="bg-bubblesR">
-                {Array.from({ length: 4 }).map((_, i) => (
-                    <li key={i}></li>
-                ))}
-            </ul>
-        </div>
-        <div class="animatemeL opacity-20">
-            <ul class="bg-bubblesL">
-                {Array.from({ length: 6 }).map((_, i) => (
-                    <li key={i}></li>
-                ))}
-            </ul>
-        </div>
-    </>
-);
+const LiveBackground = () => {
+  const [stars, setStars] = useState([]);
 
-export default LiveBacground;
+  useEffect(() => {
+    const createStars = () => {
+      const newStars = [];
+      for (let i = 0; i < 100; i++) {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        const size = Math.random() * 5;
+        const delay = Math.random() * 1;
+        newStars.push({ x, y, size, delay });
+      }
+      setStars(newStars);
+    };
+
+    createStars();
+    window.addEventListener("resize", createStars);
+
+    return () => {
+      window.removeEventListener("resize", createStars);
+    };
+  }, []);
+
+  const starAnimation = useSpring({
+    loop: { reverse: true },
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
+
+  return (
+    <div className="absolute w-full h-full overflow-hidden">
+      {stars.map((star, index) => (
+        <animated.div
+          key={index}
+          className="absolute bg-yellow-500 rounded-full"
+          style={{
+            left: star.x,
+            top: star.y,
+            width: `${star.size}px`,
+            height: `${star.size}px`,
+            animationDelay: `${star.delay}ms`,
+            ...starAnimation,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default LiveBackground;
