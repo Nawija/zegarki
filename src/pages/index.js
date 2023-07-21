a9c38a5c802755a1525af6f428fa5569cf6ef851
Valkon <96625039+Nawija@users.noreply.github.com>
2023-07-21 18:42:12 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { Link } from "gatsby";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -12,7 +13,7 @@ const IndexPage = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -20,12 +21,12 @@ const IndexPage = () => {
     var settingsCircle = {
         centerPadding: "100px",
         dots: true,
-        slickPrev: 'flex',
+        slickPrev: "flex",
         infinite: true,
         speed: 500,
         autoplaySpeed: 3000,
-        slidesToShow: 3,
-        arrows:true,
+        slidesToShow: 4,
+        arrows: true,
         slidesToScroll: 1,
     };
 
@@ -34,37 +35,45 @@ const IndexPage = () => {
             <Slider className=" bg-gray-200/70 py-12 text-center" {...settings}>
                 <StaticImage
                     quality={100}
-                    className="h-96 max-w-screen-2xl mx-auto rounded-lg"
-                    src="https://images.pexels.com/photos/7957758/pexels-photo-7957758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    layout="fullWidth"
+                    className="max-w-screen-2xl mx-auto rounded-lg"
+                    src="https://mebloo.pl/img/dostawa_www_3.jpg"
                 />
 
                 <StaticImage
                     quality={100}
-                    className="h-96 max-w-screen-2xl mx-auto rounded-lg"
-                    src="../images/tło.jpg"
+                    className="max-w-screen-2xl mx-auto rounded-lg"
+                    src="https://mebloo.pl/img/gala19_www.jpg"
                 />
 
                 <StaticImage
                     quality={100}
-                    className="h-96 max-w-screen-2xl mx-auto rounded-lg"
-                    src="../images/tło.jpg"
+                    className="max-w-screen-2xl mx-auto rounded-lg"
+                    src="https://mebloo.pl/img/Ortensiawww.jpg"
                 />
 
                 <StaticImage
                     quality={100}
-                    className="h-96 max-w-screen-2xl mx-auto rounded-lg"
-                    src="../images/tło.jpg"
+                    className="max-w-screen-2xl mx-auto rounded-lg"
+                    src="https://mebloo.pl/img/meblowy_szok_cenowy_nowy.jpg"
                 />
             </Slider>
             <Slider
                 className="py-12 text-center max-w-screen-2xl mx-auto"
                 {...settingsCircle}
             >
+                <Link to="/meble" className="rounded-full w-max h-max bg-red-100">
+                    <StaticImage
+                        quality={100}
+                        src="https://cdn-icons-png.flaticon.com/128/5564/5564823.png"
+                    />
+                </Link>
+
                 <div>
                     <StaticImage
                         quality={100}
                         className="h-40 w-40 max-w-screen-2xl rounded-full"
-                        src="https://images.pexels.com/photos/7957758/pexels-photo-7957758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        src="https://mebloo.pl/img/gala19_www.jpg"
                     />
                 </div>
 
@@ -72,15 +81,7 @@ const IndexPage = () => {
                     <StaticImage
                         quality={100}
                         className="h-40 w-40 max-w-screen-2xl rounded-full"
-                        src="https://images.pexels.com/photos/7957758/pexels-photo-7957758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    />
-                </div>
-
-                <div>
-                    <StaticImage
-                        quality={100}
-                        className="h-40 w-40 max-w-screen-2xl rounded-full"
-                        src="https://images.pexels.com/photos/7957758/pexels-photo-7957758.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        src="https://mebloo.pl/img/Ortensiawww.jpg"
                     />
                 </div>
 
